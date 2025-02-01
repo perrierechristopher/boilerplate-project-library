5a77983f66b7d8c74b07f7731d355bdb25e0742d
Dany Christopher PERRIERE <perrierechristopher@gmail.com>
2025-02-01 14:07:41 +0000

    First commit

--- routes/api.js
+++ routes/api.js
@@ -8,21 +8,91 @@
 
 'use strict';
 
+const _data = require("../lib/data");
+const helpers = require("../lib/helpers");
+
 module.exports = function (app) {
 
   app.route('/api/books')
     .get(function (req, res){
       //response will be array of book objects
+      _data.list("books", async (err, data) => {
+        if (!err) {
+          const newData = await data.map(d=>({
+            _id: d._id,
+            title: d.title,
+            commentcount: d.comments.length
+          }))
+          res.json(newData);
+        } else {
+          console.log(err)
+          res.send("An error occurred while reading the data")
+        }
+      })
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
     
     .post(function (req, res){
-      let title = req.body.title;
-      //response will contain new book object including atleast _id and title
+      
+      try {
+        let title =
+        typeof req.body?.title === "string" &&
+        req.body?.title.trim().length > 0
+          ? req.body?.title.trim()
+          : false;
+        //response will contain new book object including atleast _id and title
+        if(!title){
+          return res.send("missing required field title")
+        }
+  
+        let _id = helpers.createRandomString(24)
+        _data.read("books", _id, function (err, data){
+          if(err) {
+            let book = {
+              _id,
+              title,
+              comments: []
+            }
+  
+            _data.create("books", _id, book, (err)=> {
+              if(!err) {
+                const returnedData = {
+                  ...book,
+                  comments: undefined,
+                }
+                res.json(returnedData)
+              } else {
+                console.log(err)
+                return res.send("Could not create book")
+              }
+            })
+  
+          } else {
+            return res.send("Internal Server Error")
+          }
+        })
+      } catch (err) {
+        console.log(err)
+        return res.send("Internal Server Error")
+      }
+
+
     })
     
     .delete(function(req, res){
       //if successful response will be 'complete delete successful'
+      _data.list("books", async function(err, books){
+        if(!err) {
+          await books.forEach((d)=> {
+             _data.delete("books", d._id, function(err){
+            })
+          
+          });
+          return res.send("complete delete successful")
+        } else {
+          return res.send("could not read books")
+        }
+      })
     });
 
 
@@ -30,6 +100,14 @@ module.exports = function (app) {
   app.route('/api/books/:id')
     .get(function (req, res){
       let bookid = req.params.id;
+
+      _data.read("books", bookid, function (err, data){
+        if(!err) {
+          res.json(data)
+        } else {
+          res.send("no book exists")
+        }
+      })
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
     
@@ -37,11 +115,45 @@ module.exports = function (app) {
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
+      if (!comment) {
+        return res.send("missing required field comment")
+      }
+      _data.read("books", bookid, function (err, bookData){
+        if(!err) {
+          const newBookData = {
+            ...bookData,
+            comments: [...bookData.comments, comment]
+          }
+          _data.update("books", bookid, newBookData, (err, bookData) => {
+            if(!err) {
+              res.send(newBookData)
+            } else {
+              return res.send("could not update book")
+            }
+          })
+        } else {
+          res.send("no book exists")
+        }
+      })
     })
     
     .delete(function(req, res){
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
+      _data.read("books", bookid, function(err, bookData){
+        if(!err) {
+          _data.delete("books", bookid, function(err){
+            if(!err) {
+              res.send("delete successful")
+            } else {
+              res.send("could not delete book")
+            }
+          })
+        } else {
+          res.send("no book exists")
+        }
+      })
+
     });
-  
+
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -41,33 +41,72 @@ suite('Functional Tests', function() {
     suite('POST /api/books with title => create book object/expect book object', function() {
       
       test('Test POST /api/books with title', function(done) {
-        //done();
+        chai
+        .request(server)
+        .post('/api/books')
+        .send({
+          title: "Something nice"
+        })
+        .end((err, res) => {
+          assert.hasAllKeys(res.body, ["title", "_id"]);
+        })
+        done();
       });
       
       test('Test POST /api/books with no title given', function(done) {
-        //done();
+        chai
+        .request(server)
+        .post('/api/books')
+        .send({
+        })
+        .end((err, res) => {
+          assert.equal(res.text, "missing required field title");
+        })
+        done();
+      });
+
       });
       
-    });
 
 
     suite('GET /api/books => array of books', function(){
       
       test('Test GET /api/books',  function(done){
-        //done();
+        chai
+        .request(server)
+        .get('/api/books')
+        .end(async (err, res) => {
+          assert.isArray(res.body);
+          res.body.forEach(function(item){
+              assert.hasAllKeys(item, ["title", "_id", "commentcount"]);
+          })
+        })
+        done();
       });      
+    });      
       
-    });
 
 
     suite('GET /api/books/[id] => book object with [id]', function(){
       
       test('Test GET /api/books/[id] with id not in db',  function(done){
-        //done();
+        chai
+        .request(server)
+        .get('/api/books/htherge')
+        .end(async (err, res) => {
+          assert.equal(res.text, "no book exists");
+        })
+        done();
       });
       
       test('Test GET /api/books/[id] with valid id in db',  function(done){
-        //done();
+        chai
+        .request(server)
+        .get('/api/books/ofh7zocx74hvjyvqx7tuwd9z')
+        .end(async (err, res) => {
+          assert.hasAllKeys(res.body, ["title", "_id"]);
+        })
+        done();
       });
       
     });
@@ -76,15 +115,39 @@ suite('Functional Tests', function() {
     suite('POST /api/books/[id] => add comment/expect book object with id', function(){
       
       test('Test POST /api/books/[id] with comment', function(done){
-        //done();
+        chai
+          .request(server)
+          .post('/api/books/ofh7zocx74hvjyvqx7tuwd9z')
+          .send({
+            comment: "This book is good"
+          }).end((err, res)=>{
+            assert.hasAllDeepKeys(res.body, ["title", "_id", "comments"])
+            assert.isArray(res.body.comments)
+          })
+        done();
       });
 
       test('Test POST /api/books/[id] without comment field', function(done){
-        //done();
+        chai
+          .request(server)
+          .post('/api/books/ofh7zocx74hvjyvqx7tuwd9z')
+          .send({
+          }).end((err, res)=>{
+            assert.equal(res.text, "missing required field comment")
+          })
+        done();
       });
 
       test('Test POST /api/books/[id] with comment, id not in db', function(done){
-        //done();
+        chai
+          .request(server)
+          .post('/api/books/ofh7zocx74hvjyvqx7tuwd9')
+          .send({
+            comment: "This comment"
+          }).end((err, res)=>{
+            assert.equal(res.text, "no book exists")
+          })
+        done();
       });
       
     });
@@ -92,11 +155,27 @@ suite('Functional Tests', function() {
     suite('DELETE /api/books/[id] => delete book object id', function() {
 
       test('Test DELETE /api/books/[id] with valid id in db', function(done){
-        //done();
+        Promise.all([
+          chai.request(server).get("/api/books")
+        ]).then(values=>{
+          chai
+            .request(server)
+            .delete(`/api/books/${values[0].body[0]?._id}`)
+            .end((err, res)=>{
+             assert.equal(res.text, "delete successful")
+            })
+        })
+        done();
       });
 
       test('Test DELETE /api/books/[id] with  id not in db', function(done){
-        //done();
+        chai
+          .request(server)
+          .delete('/api/books/ofh7vqx7tuwd9')
+          .end((err, res)=>{
+            assert.equal(res.text, "no book exists")
+          })
+        done();
       });
 
     });
